51a249a61ae4973eda509d52f9b93e9d087a4c82
Cameron Morgan <cameron.morgan@skiff.com>
2023-01-28 11:46:29 -0800

    Completed Scope and Return Statement challenges.

---
+++ challengeScope.js
@@ -0,0 +1,18 @@
+// Challenge Exercises:
+
+// Create a Function called 'sum'. It will take two parameters
+// call these parameters 'num1' and 'num2'. Return the two parameters added together.
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(sum(99, 1));
+
+// Create a function called 'sayMyName'. It will take one parameter. Call this
+// parameter 'myName'. Return the phrase "Hello, my name is " and the myName parameter.
+// eg: if name is 'Dan' it should return the string: 'Hello, my name is Dan'.
+function sayMyName(myName) {
+  return `Hello, my name is ${myName}.`;
+}
+
+console.log(sayMyName("Dan"));
